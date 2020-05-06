540d951ee555df05b31099fdfa93eec2f36b1a03
chickenfingerwu <32426957+chickenfingerwu@users.noreply.github.com>
2020-05-07 01:52:16 +0700

    Update download_cyclegan_dataset_local.sh

--- datasets/download_cyclegan_dataset_local.sh
+++ datasets/download_cyclegan_dataset_local.sh
@@ -1,4 +1,5 @@
 FILE=$1
+JNTOKEN=$2
 
 if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" &&  $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "mini" && $FILE != "mini_pix2pix" && $FILE != "mini_colorization" && $FILE != "dali2photo"]]; then
     echo "Available datasets are: apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, cezanne2photo, ukiyoe2photo, vangogh2photo, maps, cityscapes, facades, iphone2dslr_flower, ae_photos, dali2photo"
@@ -12,7 +13,7 @@ if [[ $FILE == "cityscapes" ]]; then
 fi
 
 echo "Specified [$FILE]"
-URL=http://localhost:8888/$FILE.zip
+URL=http://localhost:8888/datasets/$FILE.zip?download=1&token=$JNTOKEN
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
